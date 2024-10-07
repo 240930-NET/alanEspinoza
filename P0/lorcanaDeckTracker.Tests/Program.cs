83d1e321b11c56ca8fcf4cfa005795586c60b948
alan <alankevespinoza@gmail.com>
2024-10-07 12:09:46 -0500

    test update

--- P0/lorcanaDeckTracker.Tests/Program.cs
+++ P0/lorcanaDeckTracker.Tests/Program.cs
@@ -1,9 +1,15 @@
 ﻿namespace lorcanaDeckTracker.Tests;
+using lorcanaDeckTracker.App;
+using System.Drawing;
 
 public class UnitTest0
 {
-    [Fact(Skip = "Test Template")]
-    public void Test0(){
-        
+    [Fact(Skip = "Not Working Yet")]
+    public void DeckListAccurate(){
+
+        // string boolString = DeckList.Exists(x=> x.Name = "Agro Amber Steel");
+        // bool result = bool.Parse(boolString.ToLower());
+        // Assert.True(result);
+
     }
 }
